57bd0114e5ff8e316c222a59357876479fe6c86f
NnatasaK <natasakolaric2@gmail.com>
2023-10-26 17:01:27 +0200

    same

--- Blogg-material/main-page.js
+++ Blogg-material/main-page.js
@@ -28,3 +28,20 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+/* slideshow for first page */
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+};
